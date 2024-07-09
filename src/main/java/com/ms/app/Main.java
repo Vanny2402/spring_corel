f36cc857da6245c7d437bcfa1d48d77c07bb1af9
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-10 06:40:07 +0700

    End of D31

--- src/main/java/com/ms/app/Main.java
+++ src/main/java/com/ms/app/Main.java
@@ -2,6 +2,7 @@ package com.ms.app;
 
 import com.ms.app.model.JavaTeacher;
 
+
 public class Main {
     public static void main(String[] args) {
         JavaTeacher jvt=new JavaTeacher("VANNY","JAVA");

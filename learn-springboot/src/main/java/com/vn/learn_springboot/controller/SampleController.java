9daa0134637a04d9f81cfc50f17537f119c094fa
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-13 13:52:13 +0700

    End of 44 Sample Reqequest

---
+++ learn-springboot/src/main/java/com/vn/learn_springboot/controller/SampleController.java
@@ -0,0 +1,26 @@
+package com.vn.learn_springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Sample")
+public class SampleController {
+
+	@GetMapping
+	public String getGreeting() {
+		return "Hello marry";
+	}
+	
+	@GetMapping("/inside")
+	public String getInsideApplicatin() {
+		return "Hi This is Inside the Application";
+	}
+	
+	@GetMapping("/outside")
+	public String getOutsideApplicatin() {
+		return "Hi This is outside the Application";
+	}
+}
+

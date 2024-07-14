968f5448b157e2278921030a3b17f78c616e2af7
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-14 14:45:36 +0700

    End of 48

--- learn-springboot/src/main/java/com/vn/learn_springboot/controller/BooController.java
+++ learn-springboot/src/main/java/com/vn/learn_springboot/controller/BooController.java
@@ -1,5 +1,8 @@
 package com.vn.learn_springboot.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,24 @@ public class BooController {
 	@GetMapping("/{id}/{age}")
 	public String getBooks(@PathVariable Long id, @PathVariable String age) {
 		return "Hello Id: "+id+"age: "+age;
+	}
+	
+	@GetMapping("/{id}/{age}/{address}")
+	public String getBook(@PathVariable Map<String,String> map) {
+		String id=map.get("id");
+		String age=map.get("age");
+		String address=map.get("address");
+		return "Here is Data ID:  "+id +" Age: "+age+ " Address: "+address;
 		
 		
+	}
+	
+	@GetMapping("/{id}")
+	public String getBooks3(@PathVariable(value="id") Optional<Long> bookId) {
+		if(bookId.isPresent()) {
+			return "Book id: "+bookId.get()+" Is still present";
+		}else {
+			return"There is no this book";
+		}
 	}
 }

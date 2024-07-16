edfb2eab5abc7cdf28098a3532607eac5ca56fc7
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-16 18:38:59 +0700

    End of D55

--- learn-springboot/src/main/java/com/vn/learn_springboot/model/entity/StudentEntity.java
+++ learn-springboot/src/main/java/com/vn/learn_springboot/model/entity/StudentEntity.java
@@ -4,14 +4,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="students",schema = "student")
 public class StudentEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long id;
+	private Long id;
+	private String name;
 	
-	public String name;
 	
+	public void setId(Long id) {
+		this.id=id;
+	}
+	public Long getId(){
+
+		return this.id;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }

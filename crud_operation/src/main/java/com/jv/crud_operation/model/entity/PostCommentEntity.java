1370659986046d74f6645c461bf0efe5bce6c54e
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-08-07 18:16:33 +0700

    End of D85

--- crud_operation/src/main/java/com/jv/crud_operation/model/entity/PostCommentEntity.java
+++ crud_operation/src/main/java/com/jv/crud_operation/model/entity/PostCommentEntity.java
@@ -2,6 +2,7 @@ package com.jv.crud_operation.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,7 +21,7 @@ public class PostCommentEntity {
 	@Column(nullable = false,length = 250)
 	private String comment;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="postId",nullable = false,referencedColumnName = "id")
 	private PostEntity post;
 	
--- crud_operation/src/main/java/com/jv/crud_operation/model/entity/response/comment/CommentResponse.java
+++ crud_operation/src/main/java/com/jv/crud_operation/model/entity/response/comment/CommentResponse.java
@@ -3,11 +3,11 @@ package com.jv.crud_operation.model.entity.response.comment;
 import java.io.Serializable;
 
 import com.jv.crud_operation.model.entity.PostCommentEntity;
+import com.jv.crud_operation.model.entity.response.post.ShotPostResponse;
 
 public class CommentResponse implements Serializable {
 
-
-
+	private ShotPostResponse post;
 	private Long id;
 	private String comment;
 	
@@ -24,13 +24,16 @@ public class CommentResponse implements Serializable {
 		this.comment = comment;
 	}
 
-	public CommentResponse(Long id, String comment) {
+	public ShotPostResponse getPost() {
+		return post;
+	}
+	public CommentResponse(Long id, String comment,ShotPostResponse post) {
 		this.id = id;
 		this.comment = comment;
+		this.post=post;
 	}
 
-	
 	public static CommentResponse fromEntity(PostCommentEntity entity) {
-		return new CommentResponse(entity.getId(), entity.getComment());
+		return new CommentResponse(entity.getId(), entity.getComment(),ShotPostResponse.fromEntity(entity.getPost()));
 	}
 }

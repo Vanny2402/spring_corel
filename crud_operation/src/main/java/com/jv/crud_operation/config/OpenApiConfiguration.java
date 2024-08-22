fcdda1bd4563473019ec9951260b9db89a869489
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-08-22 08:02:08 +0700

    end of D117

---
+++ crud_operation/src/main/java/com/jv/crud_operation/config/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package com.jv.crud_operation.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+
+	@Bean
+	public GroupedOpenApi backEndGroup() {
+		return GroupedOpenApi.builder().group("backend-api").addOpenApiCustomizer(openApi-> openApi.info(getBanendApiInfo())).packagesToScan("com.jv.crud_operation.controller").build();
+	}
+	
+	@Bean
+	public GroupedOpenApi fornEndGroup() {
+		return GroupedOpenApi.builder().group("frontEnd-api").addOpenApiCustomizer(openApi-> openApi.info(getBanendApiInfo())).packagesToScan("com.jv.crud_operation").build();
+	}
+	
+	private Info getBanendApiInfo() {
+		Contact contact=new Contact();
+		contact.setName("Vanny");
+		contact.setEmail("Vanny@gmail.com");
+		contact.setUrl("https://github.com/Vanny2402/spring_corel");
+		return new Info().title("Admin").version("1.0.1").description("Hi Admin").contact(contact);
+	}
+}
